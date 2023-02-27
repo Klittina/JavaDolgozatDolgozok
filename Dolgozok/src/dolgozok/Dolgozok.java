4b88e11393b2100160d17abf956a4e4b36feada9
Szedlár Krisztina Mercédesz <szedlar.k.mercedesz@dk.akkszalezi.hu>
2023-02-27 13:50:29 +0100

    fájlbeolvasás hibák

--- Dolgozok/src/dolgozok/Dolgozok.java
+++ Dolgozok/src/dolgozok/Dolgozok.java
@@ -1,9 +1,36 @@
 package dolgozok;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Dolgozok {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    FileReader fileReader = new FileReader("../txt/emberek.txt");
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+    
     }
+
+    
+//    public static void main(String[] args) {
+//        try {
+//      File myObj = new File("txt/emberek.txt");
+//      Scanner myReader = new Scanner(myObj);
+//      while (myReader.hasNextLine()) {
+//        String data = myReader.nextLine();
+//        System.out.println(data);
+//      }
+//      myReader.close();
+//    } catch (FileNotFoundException e) {
+//      System.out.println("An error occurred.");
+//      e.printStackTrace();
+//    }
+//  }
+
     
 }
